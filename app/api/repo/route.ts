377ec300d7813a01eb9607c58e831bb07ff2811a
Guy Korland <gkorland@gmail.com>
2024-01-07 16:44:48 +0200

    move to parsers folder

--- app/api/repo/route.ts
+++ app/api/repo/route.ts
@@ -467,12 +467,12 @@ export async function POST(request: NextRequest) {
 	// Initialize Tree-Sitter parser
 	await Parser.init({
 		locateFile(scriptName: string, scriptDirectory: string) {
-			return path.join(process.cwd(), 'app/api/repo', scriptName);
+			return path.join(process.cwd(), 'app/parsers', scriptName);
 		},
 	});
 
 	parser = new Parser();
-	const Python = await Parser.Language.load(path.join(process.cwd(), 'app/api/repo/tree-sitter-python.wasm'));
+	const Python = await Parser.Language.load(path.join(process.cwd(), 'app/parsers/tree-sitter-python.wasm'));
 
 	parser.setLanguage(Python);
 
