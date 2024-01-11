5fd8145eddf397e5acbef508a6b779745fefffda
swilly22 <roilipman@gmail.com>
2024-01-11 20:49:49 +0200

    add additional repos

--- app/components/repositories.ts
+++ app/components/repositories.ts
@@ -1,4 +1,7 @@
 export const RESPOSITORIES = [
     "https://github.com/falkordb/falkordb-py",
     "https://github.com/pallets/click",
+    "https://github.com/pallets/flask",
+    "https://github.com/psf/requests",
+    "https://github.com/wention/BeautifulSoup4",
 ]
