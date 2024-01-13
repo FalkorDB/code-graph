d30a59a0dd078f2ddf8e860994cbf6f3847579ba
swilly22 <roilipman@gmail.com>
2024-01-13 23:13:13 +0200

    fix graph project query

--- app/api/repo/questions.ts
+++ app/api/repo/questions.ts
@@ -1,4 +1,7 @@
 export const QUESTIONS = [
+    "List a few recursive functions",
     "What is the name of the most used method?",
     "Who is calling the most used method?",
-]
+    "Which function has the largest number of arguments? List a few arguments",
+    "Show a calling path between the drop_edge_range_index function and _query, only return function(s) names",
+];
