59288b345ca91d0950ae70096df54bb9110aa4b1
Naseem Ali <34807727+Naseem77@users.noreply.github.com>
2024-11-06 18:19:43 +0200

    init (#166, #167, #168, #171)

---
+++ e2e/infra/api/apiRequests.ts
@@ -0,0 +1,24 @@
+import { APIRequestContext, request } from "@playwright/test"
+
+
+const getRequest = async (url: string, body: any, availableRequest?: APIRequestContext, headers?: Record<string, string>) => {
+  const requestOptions = {
+    data: body,
+    headers: headers || undefined,
+  };
+
+  const requestContext = availableRequest || (await request.newContext());
+  return await requestContext.get(url, requestOptions);
+};
+
+const postRequest = async (url: string, body: any, availableRequest?: APIRequestContext, headers?: Record<string, string>) => {
+  const requestOptions = {
+    data: body,
+    headers: headers || undefined,
+  };
+
+  const requestContext = availableRequest || (await request.newContext());
+  return await requestContext.post(url, requestOptions);
+};
+
+export{ getRequest, postRequest }
