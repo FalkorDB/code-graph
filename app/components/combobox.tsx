2e7e11c1ef0d207134f6e232d77895d9f97d8590
Anchel135 <yakoveidelman1@gmail.com>
2024-12-02 11:57:44 +0200

    fetch options when select opens after 30 sec that he was close expat for the first time

--- app/components/combobox.tsx
+++ app/components/combobox.tsx
@@ -1,15 +1,56 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { toast } from "@/components/ui/use-toast";
+import { useEffect, useState } from "react";
 
 interface Props {
     options: string[]
+    setOptions: (options: string[]) => void
     selectedValue: string
     onSelectedValue: (value: string) => void
 
 }
 
-export default function Combobox({ options, selectedValue, onSelectedValue }: Props) {
+export default function Combobox({ options, setOptions, selectedValue, onSelectedValue }: Props) {
+
+    const [open, setOpen] = useState(false)
+    const [lastOpened, setLastOpened] = useState<number>();
+
+    const fetchOptions = async () => {
+        const result = await fetch(`/api/repo`, {
+            method: 'GET',
+        })
+
+        if (!result.ok) {
+            toast({
+                variant: "destructive",
+                title: "Uh oh! Something went wrong.",
+                description: await result.text(),
+            })
+            return
+        }
+
+        const json = await result.json()
+        setOptions(json.result)
+    }
+
+    useEffect(() => {
+        fetchOptions()
+    }, [])
+
+    useEffect(() => {
+        if (!open) return
+
+        const now = Date.now();
+
+        if (lastOpened && now - lastOpened < 30000) return;
+
+        setLastOpened(now);
+
+        fetchOptions()
+    }, [open])
+
     return (
-        <Select value={selectedValue} onValueChange={onSelectedValue}>
+        <Select open={open} onOpenChange={setOpen} value={selectedValue} onValueChange={onSelectedValue}>
             <SelectTrigger className="rounded-md border focus:ring-0 focus:ring-offset-0">
                 <SelectValue placeholder="Select a repo" />
             </SelectTrigger>
