ec9ec3b3cdc39b993699b766f170c848123f5775
Anchel135 <yakoveidelman1@gmail.com>
2024-12-24 13:15:23 +0200

    add intercom

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
-import GoogleAnalytics from './components/GoogleAnalytics'
 import { cn } from '@/lib/utils'
 
+import GTM from './GTM'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -20,10 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, "relative")}>
-        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
-          <GoogleAnalytics ga_id=
-            {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
-        ) : null}
+        <GTM />
         {children}
         <Toaster />
       </body>
