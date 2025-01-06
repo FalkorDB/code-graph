3cf7586f51fa75d3208ecf3354a8d27d43cbc5a9
Anchel135 <yakoveidelman1@gmail.com>
2025-01-06 14:43:58 +0200

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -79,6 +79,7 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        'spin': 'spin 1s linear',
       },
     },
   },
