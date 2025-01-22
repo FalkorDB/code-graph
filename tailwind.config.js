17d8fd85ca4ef3b600e91b388b68b2d036bfd9d1
Guy Korland <gkorland@gmail.com>
2025-01-22 10:38:36 +0200

    Remove 'spin' animation from config

--- tailwind.config.js
+++ tailwind.config.js
@@ -79,7 +79,6 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        'spin': 'spin 1s linear',
       },
     },
   },
